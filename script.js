f9cf07c674b63b3350aa15020e2a9583d73139fe
Doom <dustinhm607@gmail.com>
2025-08-14 09:51:27 -0700

    add checkWinner method to game object

--- script.js
+++ script.js
@@ -71,8 +71,26 @@ const game = (function(){
     }
 
     const checkWinner = function(){
+        const wins = [
+            [0, 1, 2], // Top row
+            [3, 4, 5], // Middle row
+            [6, 7, 8], // Bottom row
+            [0, 3, 6], // Left column
+            [1, 4, 7], // Middle column
+            [2, 5, 8], // Right column
+            [0, 4, 8], // Diagonal
+            [2, 4, 6]  // Diagonal
+        ];
 
+        for (const [a, b, c] of wins){
+            if ( typeof gameboard.getPosition(a) != 'number' && gameboard.getPosition(a) == gameboard.getPosition(b) && gameboard.getPosition(a) == gameboard.getPosition(c)){
+                return gameboard.getPosition(a);
+            }
+            else {
+                return null;
+            }
+        }
     }
 
-    return {playRound};
+    return {playRound, checkWinner};
 })()
