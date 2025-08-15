42c5a02565b197bca2f3fdd8bff0b70bb8644676
Doom <97200707+Doomguy504@users.noreply.github.com>
2025-08-14 23:00:28 -0700

    create game cells

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const gameboard = (function(){
-    const board = [
-        0, 1, 2,
-        3, 4, 5,
-        6, 7, 8
-    ];
+    const gameContainer = document.querySelector('.game-container');
+    const board = [];
+ 
+    for (let i = 0; i < 9; i++ ){
+        const cell = document.createElement('div');
+
+        cell.setAttribute('class', 'cell');
+        gameContainer.appendChild(cell);
+        
+        board[i] = cell;
+    }
 
     const getBoard = () => board;
 
